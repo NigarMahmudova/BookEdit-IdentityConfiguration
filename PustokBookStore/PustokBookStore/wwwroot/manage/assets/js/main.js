93658ae7843a8a0831c1ec4148aabb1807146fba
NigarMahmudova <nigarem@code.edu.az>
2023-06-30 06:47:12 -0700

    added book edit and identity configuration

---
+++ PustokBookStore/PustokBookStore/wwwroot/manage/assets/js/main.js
@@ -0,0 +1,34 @@
+﻿
+$(document).on("click", "delete-btn", function (e) {
+    e.preventDefault();
+    let url = $(this).attr("href");
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            fetch(url).then(response => {
+                if (response.ok) {
+                    Swal.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                    ).then(() => {
+                        window.location.reload();
+                    })
+                }
+                else if (response.status == 400) {
+                    alert("Bu genre ile elaqeli kitablar var!")
+                }
+                else {
+                    alert("Xeta bas verdi!")
+                }
+            })   
+        }
+    })
+})
